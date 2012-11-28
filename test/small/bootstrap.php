ca0ad2e3cceb2eae7795833662693e9fcef45564
Moritz Schmidt <fusselwurm@gmail.com>
2012-11-28 10:28:37 +0100

    add 'does it run' test for Respect/Validation/Validator. include autoloader in bootstrap.php .

--- test/small/bootstrap.php
+++ test/small/bootstrap.php
@@ -4,3 +4,7 @@ require_once $basedir . 'SplClassLoader.php';
 
 $classLoader = new SplClassLoader('Org\\Plista\\Curd', $basedir);
 $classLoader->register();
+
+$classLoader = new SplClassLoader('Respect\\Validation', "$basedir/Validation/library/");
+$classLoader->register();
+
